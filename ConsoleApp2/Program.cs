17a8f5ad23a98ea4426364ae867f1ff2a2ea88c0
ilkhvm <ilkhvm@gmail.com>
2024-08-12 19:11:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        string MyName;
+        MyName = "Milana";
+
+        Console.WriteLine(MyName);
+
+        Console.ReadKey();
+    }
+}
